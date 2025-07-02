657f9d8b2c17e1c689b5e657b47d7bbe6d253dd5
shreyd04 <officialsd27204@gmail.com>
2025-07-02 09:11:24 +0000

    a variable file is added

---
+++ .devcontainer/basic_01/01_variables.js
@@ -0,0 +1,6 @@
+const accountID = 14532;
+let accountEmail = "abc@.com";
+var accountPassword = "1234566890";
+console.log(accountID = 0);
+//but this is quiet a lengthy task so we will do table method
+console.table(accountID , accountEmail = "abd@.com" , accountPassword);
