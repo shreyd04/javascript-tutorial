c0d78fb93c237b24df662cf8eb7243be2aac9943
shreyd04 <officialsd27204@gmail.com>
2025-07-23 18:36:47 +0000

    update

--- .devcontainer/basic_01/comparison.js
+++ .devcontainer/basic_01/comparison.js
@@ -9,3 +9,8 @@ console.log(userOne.userId); // accessing object property
 let userfunc = userOne;
 userfunc.userId = "abc";
 console.log(userOne.userId); // accessing object property after modification
+const name  = "j";
+const repc = 1;
+console.log(name + repc + "only"); // string concatenation
+// but in modern day we use string interpolation wherewe make
+//placeholders to keep the string in a single line
