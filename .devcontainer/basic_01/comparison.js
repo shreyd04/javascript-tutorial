71b6f63cfdacb4b66019b857fd98fe67c7aa96d4
shreyd04 <officialsd27204@gmail.com>
2025-07-23 18:14:25 +0000

    updated

--- .devcontainer/basic_01/comparison.js
+++ .devcontainer/basic_01/comparison.js
@@ -1,1 +1,11 @@
 console.log(null == 0);
+
+let userOne = {
+    userId: "1234@gmail.com", // this is heap memory
+    userNo:  4
+}
+console.log(userOne.userId); // accessing object property
+
+let userfunc = userOne;
+userfunc.userId = "abc";
+console.log(userOne.userId); // accessing object property after modification
