31f31bf9d59e4506e4ea63d0e133b21276d82d64
shreyd04 <officialsd27204@gmail.com>
2025-07-30 08:43:38 +0000

    new file

--- .devcontainer/basic_01/arrays.js
+++ .devcontainer/basic_01/arrays.js
@@ -1,1 +1,2 @@
-// array copy operation 
+// array copy operation : makes shallow copy their referece is same they share it means the changes will take place in the original array
+// deep copy means it doesn't share the same reference
