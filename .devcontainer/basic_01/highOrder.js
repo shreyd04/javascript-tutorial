6d1785d80933a3a7e8002bccc73293821e168dfe
shreyd04 <officialsd27204@gmail.com>
2025-08-07 09:58:55 +0000

    key values not working in for -of. loop for map

--- .devcontainer/basic_01/highOrder.js
+++ .devcontainer/basic_01/highOrder.js
@@ -1,5 +1,36 @@
 
-const map = [1, 2 , 3 , 4 , 5];
-for(const num of map){
+const array = [1, 2 , 3 , 4 , 5];
+for(const num of array){
    console.log(num);
 }
+// Maps
+const map = new Map();
+map.set('a' , 1);
+map.set('b' , 2);
+map.set('c' , 3);
+map.set('d' , 4);
+console.log(map);
+ 
+/*for(const [key , value] of map){
+ //console.log(`${key} : ${value}`);
+} this doesn't run*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
