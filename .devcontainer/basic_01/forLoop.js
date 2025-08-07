98efbc1404ae169bfe6d8994e38570efdcac5a7a
shreyd04 <officialsd27204@gmail.com>
2025-08-07 09:31:13 +0000

    highorder learned

---
+++ .devcontainer/basic_01/forLoop.js
@@ -0,0 +1,7 @@
+
+for(let i = 0 ; i<10; i++){
+    const ele = i;
+    console.log(ele);
+}
+//similar to c++
+
---
+++ .devcontainer/basic_01/highOrder.js
@@ -0,0 +1,5 @@
+
+const map = [1, 2 , 3 , 4 , 5];
+for(const num of map){
+   console.log(num);
+}
